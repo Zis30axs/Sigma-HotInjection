714effd4776ce6e0ef6faae92a2f78262af09865
Zis30axs <140671528+Zis30axs@users.noreply.github.com>
2026-08-20 11:00:44 +0800

    Add Minecraft 1.8.9 adapter slot

---
+++ hotinjection-agent/src/main/java/dev/zis30axs/sigma/hotinjection/agent/version/v1_8_9/V1_8_9Adapter.java
@@ -0,0 +1,10 @@
+package dev.zis30axs.sigma.hotinjection.agent.version.v1_8_9;
+
+import dev.zis30axs.sigma.hotinjection.agent.version.AbstractVersionAdapter;
+import dev.zis30axs.sigma.hotinjection.version.MinecraftVersion;
+
+public final class V1_8_9Adapter extends AbstractVersionAdapter {
+    public V1_8_9Adapter() {
+        super(MinecraftVersion.V1_8_9);
+    }
+}
