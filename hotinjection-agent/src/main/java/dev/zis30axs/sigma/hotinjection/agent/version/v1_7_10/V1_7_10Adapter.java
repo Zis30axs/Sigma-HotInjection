c886b0fe1d81c656b7040fda23b15aecb516835d
Zis30axs <140671528+Zis30axs@users.noreply.github.com>
2026-08-20 11:00:34 +0800

    Add Minecraft 1.7.10 adapter slot

---
+++ hotinjection-agent/src/main/java/dev/zis30axs/sigma/hotinjection/agent/version/v1_7_10/V1_7_10Adapter.java
@@ -0,0 +1,10 @@
+package dev.zis30axs.sigma.hotinjection.agent.version.v1_7_10;
+
+import dev.zis30axs.sigma.hotinjection.agent.version.AbstractVersionAdapter;
+import dev.zis30axs.sigma.hotinjection.version.MinecraftVersion;
+
+public final class V1_7_10Adapter extends AbstractVersionAdapter {
+    public V1_7_10Adapter() {
+        super(MinecraftVersion.V1_7_10);
+    }
+}
